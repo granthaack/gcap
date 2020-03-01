b5958e4346c533a7a7002bb42c92c4a18513bc12
granthaack <granthaack@protonmail.com>
2020-03-01 01:52:18 -0800

    got a basic module laid out for the DRAM

---
+++ rtl/gcap/gcap.srcs/sources_1/new/dram_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/01/2020 12:28:18 AM
+// Design Name: 
+// Module Name: dram_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dram_top(
+
+    );
+endmodule
